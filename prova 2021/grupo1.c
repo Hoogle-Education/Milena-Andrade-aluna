6a904184638d82cd3fa7e171dbfd17dc317d1e48
Hugo Rafael <hugorafell@gmail.com>
2022-02-15 20:12:49 -0300

    After spellcheck

--- prova 2021/grupo1.c
+++ prova 2021/grupo1.c
@@ -1,22 +1,30 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
 
 void Divisores(int N)
 {
  int i;
 
- for(i=2; i<N/2; i++)
-  if(N%i != 0)
-    printf("%d "; i);
+  printf("%d\n", N);
+
+ for(i=1; i<=N ; i++)
+  if(N%i == 0)
+    printf("%d ", i);
 }
 
 // dado um número inteiro N, colocar os divisores,
 // testando a divisão pelos números de 2 a N/2
-int main(int argc; char **argv)
+int main(int argc, char **argv)
 {
- 
- if(argv!=2) return;
 
- printf("Divisores: ", N);
- Divisores(atof(argv[1]));
+  if(argc != 2) return 0;
+
+  // strlen -> 1 -> caracter -> printf(caracter)
+  // strlen > 1 -> string -> ...
+
+  // printf("Divisores: %c\n", *argv[1]);
+  Divisores( (int)atof(argv[1]) );
 
 }
